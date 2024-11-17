175d4cde1dd4ab344a2c8d070dd12798703d885f
santoshkumarm1 <santoshkumarm1@verifone.com>
2024-11-17 18:13:07 +0530

    python + mongo

--- src/main.py
+++ src/main.py
@@ -11,11 +11,24 @@ def mongo_insert_demo():
     print(result)
 
 
+def mongo_insert1_demo():
+    client = MongoClient("mongodb://localhost:27017/")
+    db = client["abhidemo"]  
+    collection = db["users"] 
+    
+    username = input("Enter Username")
+    
+    document = {"name" : username, "age": 21, "id":3}
+    result = collection.insert_one(document)
+    print(result)
+
+
 
 
 def main():
     print("Mongo Demo")
-    mongo_insert_demo()
+    # mongo_insert_demo()
+    mongo_insert1_demo()
 
 
 if __name__ == "__main__":

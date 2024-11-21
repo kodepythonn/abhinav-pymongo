f16e69943d07db03ea47cbb395dbb6ecff658734
santoshkumarm1 <santoshkumarm1@verifone.com>
2024-11-21 18:20:06 +0530

    update.

--- src/dbmod/insert_demo.py
+++ src/dbmod/insert_demo.py
@@ -11,13 +11,4 @@ def mongo_insert_demo():
     print(result)
 
 
-def mongo_insert1_demo():
-    client = MongoClient("mongodb://localhost:27017/")
-    db = client["abhidemo"]  
-    collection = db["users"] 
-    
-    username = "abcd"
-    
-    document = {"name" : username, "age": 21, "id":3}
-    result = collection.insert_one(document)
-    print(result)
+
